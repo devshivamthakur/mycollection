3d3a0cb68c3b63e5c38222f92dc2169993cf9fd2
shivam thakur <64252021+itsprogrammingwithme@users.noreply.github.com>
2021-01-29 21:28:00 +0530

    Add files via upload

---
+++ Surveyur_login.php
@@ -0,0 +1,50 @@
+<?php
+
+// Importing DBConfig.php file.
+include 'connection.php';
+echo ("hii")
+// Creating connection.
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ if($con)
+ {
+     echo ("Connection Successful")
+                    // Getting the received JSON into $json variable.
+                    $json = file_get_contents('php://input');
+                    
+                    // decoding the received JSON and store into $obj variable.
+                    $obj = json_decode($json,true);
+
+                    // Populate username from JSON $obj array and store into $name.
+                    $name = $obj['uname'];
+
+                    // Populate   password from JSON $obj array and store into $password.
+                    $password = $obj['password']; 
+                    // Creating SQL query and insert the record into MySQL database table.
+                    $Sql_Query = "Select *from  Surveyur  where uname like '$name', and password like '$phone_number';";
+
+
+                    if(mysqli_query($con,$Sql_Query)){
+
+                    // If the record inserted successfully then show the message.
+                    $MSG = 'Successful' ;
+
+                    // Converting the message into JSON format.
+                    $json = json_encode($MSG);
+
+                    // Echo the message.
+                    //  echo $json ;
+
+                    }
+                    else{
+
+                    $MSG = 'not Successful' ;
+                    // Converting the message into JSON format.
+                    $json = json_encode($MSG);
+                    }
+                    mysqli_close($con);
+ }else{
+     echo "";
+ }
+
+ 
+?>
---
+++ connection.php
@@ -0,0 +1,17 @@
+
+<?php
+ 
+//Define your host here.
+$HostName = 'bgfz0b2wihu6iechxgds-mysql.services.clever-cloud.com';
+ 
+//Define your database name here.
+$DatabaseName = 'bgfz0b2wihu6iechxgds';
+ 
+//Define your database username here.
+$HostUser = 'ughaijth8ft2wpjh';
+ 
+//Define your database password here.
+$HostPass = 'OBfrsxYhWiuoAhYJeHpm';
+
+ 
+?>
