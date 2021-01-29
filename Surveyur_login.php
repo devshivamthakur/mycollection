006b003746850536966859a2195d8a73a3459cc0
shivam thakur <64252021+itsprogrammingwithme@users.noreply.github.com>
2021-01-29 21:37:20 +0530

    Add files via upload

--- Surveyur_login.php
+++ Surveyur_login.php
@@ -1,7 +1,16 @@
 <?php
 
 // Importing DBConfig.php file.
-include 'connection.php';
+$HostName = 'bgfz0b2wihu6iechxgds-mysql.services.clever-cloud.com';
+ 
+//Define your database name here.
+$DatabaseName = 'bgfz0b2wihu6iechxgds';
+ 
+//Define your database username here.
+$HostUser = 'ughaijth8ft2wpjh';
+ 
+//Define your database password here.
+$HostPass = 'OBfrsxYhWiuoAhYJeHpm';
 echo ("hii")
 // Creating connection.
  $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
